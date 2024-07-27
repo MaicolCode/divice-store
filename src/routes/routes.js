d17d4be1ee15ffe8be2367e0dcf132c7645cac13
Maicol Code <paucarjhon07@outlook.com>
2024-07-27 16:23:24 -0500

    Se crearon los respectivos formularios de registro y se agregaron algunos estilos.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -16,6 +16,8 @@ const products = [
   }
 ]
 
+// * Rutas de las vistas de la pagina principal.
+
 routes.get('/', (req, res) => {
   res.render('./page/index', { title: 'Home' })
 })
@@ -32,6 +34,8 @@ routes.get('/home', (req, res) => {
   res.render('./manage/index', { title: 'Admin-Home' })
 })
 
+// * Rutas de las vistas de gestion del sistema.
+
 routes.get('/devices', (req, res) => {
   res.render('./manage/devices', { title: 'Admin-Devices', products })
 })
@@ -42,4 +46,18 @@ routes.get('/customers', (req, res) => {
 
 routes.get('/workers', (req, res) => {
   res.render('./manage/workers', { title: 'Admin-Workers' })
+})
+
+// ** Rutas de las vistas de formularios.
+
+routes.get('/formDevices', (req, res) => {
+  res.render('./manage/forms/frm-devices', { title: 'Devices' })
+})
+
+routes.get('/formCustomers', (req, res) => {
+  res.render('./manage/forms/frm-customers', { title: 'Customers' })
+})
+
+routes.get('/formWorkers', (req, res) => {
+  res.render('./manage/forms/frm-workers', { title: 'Workers' })
 })
